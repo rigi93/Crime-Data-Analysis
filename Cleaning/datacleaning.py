8980f5b7fc9e45f87c808cbd58b392ec54ed7399
miti2611 <maj529@nyu.edu>
2017-12-12 18:09:06 -0500

    Fixed bug

--- Cleaning/datacleaning.py
+++ Cleaning/datacleaning.py
@@ -6,8 +6,7 @@ import string
 from operator import add
 from pyspark import SparkContext
 from csv import reader
-from datetime import datetime, date
-import pandas
+from datetime import datetime
 
 if __name__ == "__main__":
 	sc = SparkContext()
@@ -15,12 +14,20 @@ if __name__ == "__main__":
 	lines = sc.textFile(sys.argv[1], 1)
 	
 	lines = lines.mapPartitions(lambda x: reader(x))
-	header = lines.first()
-	lines = lines.filter(lambda x: x!=header).map(lambda x: (x[0], x[1], x[2], x[5], x[6], x[10], x[11], x[13]))
 	
+	header = lines.take(1)
+
+	lines = lines.filter(lambda x: x!=header).map(lambda x: (x[0], x[1], x[2], x[5], x[6], x[9], x[10], x[12]))
+	
+	def basetype_date(input):
+		if mat is not None:
+			return "DATETIME"
+		else:
+			return type(input)
+
 	def basetype_string(input):
 		try:
-			if type(input) is str:
+			if type(input) == str:
 				return "STRING"
 		except ValueError:
 			return type(input)		
@@ -31,14 +38,7 @@ if __name__ == "__main__":
 			return "INT"
 		except ValueError:
 			return type(input)
-	
-	def basetype_date(input):
-		mat=re.match('(\d{2}|\d{1})/(\d{2}|\d{1})/(\d{4})$', input)
-		if mat is not None:
-			return "Date"
-		else:
-			return type(input)
-		
+			
 	def semantictype_date(value):
 		mat=re.match('(\d{2}|0?[1-9])/(\d{2}|0?[1-9])/(\d{4})$', value)
 		if mat is not None:
@@ -48,8 +48,9 @@ if __name__ == "__main__":
 
 	def semantictype_boro(value):
 		try: 
-			if (len(value) > 4 and len(value) < 14 and type(value) == str):
-				return "Borough name"
+			name = value.upper()		
+			if (len(name) > 4 and len(name) < 14 and type(name) == str):
+				return "Boro name"
 			else:
 				return "Other"
 		except ValueError:	
@@ -57,17 +58,19 @@ if __name__ == "__main__":
 				
 	def semantictype_status(value):
 		try: 
-			if (len(value) > 0 and len(value) < 10 and type(value) == str):
-				return "Crime Status"
+			name = value.upper()		
+			if (len(name) > 0 and len(name) < 10 and type(name) == str):
+				return "crime status"
 			else:
 				return "Other"
 		except ValueError:	
 				return "Other"
 
 	def semantictype_crimetype(value):
-		try: 		
-			if (len(value) > 5 and len(value) < 12 and type(value) == str):
-				return "Crime Type"
+		try: 
+			name = value.upper()		
+			if (len(name) > 5 and len(name) < 12 and type(name) == str):
+				return "crime type"
 			else:
 				return "Other"
 		except ValueError:	
@@ -96,16 +99,11 @@ if __name__ == "__main__":
 		except ValueError:
 			return "INVALID"		
 		
-	def validity_time(x):
-		if x == '':
-			return "NULL"
-		try:
-			if type(x) is not datetime.date:
- 				return "VALID"
- 			else:
- 				return "INVALID"
-		except ValueError:
-			return "INVALID";
+	def validity_time(input):
+		if isinstance(input, datetime.datetime):
+			return "VALID"
+		else:
+			return "INVALID"
 			
 	def validity_boro(x):
 		x = x.upper()
@@ -151,8 +149,8 @@ if __name__ == "__main__":
 	def validity_complaint_number(x):
 		try: 
 			if x == '':
-				return "INVALID"
-			elif (len(x) == 9 and x.isdigit() and int(x) > 100000000 and int(x) < 999999999):
+				return "NULL"
+			elif (len(x) == 3 and x.isdigit() and int(x) > 100 and int(x) < 900):
 				return "VALID"
 			else:	
 				return "INVALID"
@@ -162,20 +160,22 @@ if __name__ == "__main__":
 	def toCSVLine(data):
 	  return ','.join(str(d) for d in data)			
 	
-	deliverable = lines.map(lambda x: (x[0], basetype_string(x[0]), validity_complaint_number(x[0]),\
-						x[1], semantictype_date(x[1]), validity_date(x[1]),\
+	deliverable = lines.map(lambda x: (x[0], basetype_int(x[0]), validity_complaint_number(x[0]),\
+						x[1], basetype_date(x[1]), semantictype_date(x[1]), validity_date(x[1]),\
 						x[2], validity_time(x[2]),\
 						x[3], basetype_date(x[3]), semantictype_date(x[3]), validity_date(x[3]),\
 						x[4], basetype_int(x[4]), semantictype_cd(x[4]), validity_key_cd(x[4]),\
-						x[5], semantictype_status(x[5]), validity_crime(x[5]),\
+						x[5], basetype_string(x[5]), semantictype_status(x[5]), validity_crime(x[5]),\
 						x[6], basetype_string(x[6]), semantictype_crimetype(x[6]), validity_law_category(x[6]),\
 						x[7], basetype_string(x[7]), semantictype_boro(x[7]), validity_boro(x[7])))
 
-	deliverable = deliverable.filter(lambda x: x[2] == "VALID" and x[5] == "VALID" and x[7] == "VALID" \
-								and x[11] == "VALID" and x[15] == "VALID" and x[18] == "VALID" \
-								and x[22] == "VALID" and x[26] == "VALID") \
-			.map(lambda x: (x[0], x[3], x[6], x[8], x[12], x[16], x[19],x[23]))
-
-	deliverable = deliverable.map(toCSVLine)
-	deliverable.saveAsTextFile("cleanedData.csv")	
+	result = deliverable.filter(lambda x: x[2] == "VALID" and x[6] == "VALID" and x[8] == "VALID" \
+								and x[12] == "VALID" and x[16] == "VALID" and x[20] == "VALID" \
+								and x[24] == "VALID" and x[28] == "VALID") \
+			.map(lambda x: (x[0], x[3], x[7], x[9], x[13], x[17], x[21], x[25]))
+	
+	cleaned = result.map(toCSVLine)
+	
+	cleaned.saveAsTextFile("cleaned_data.csv")
+	
 	sc.stop()
